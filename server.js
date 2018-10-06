40a1daf277c32bde369ede37e76a3dc0dbc5c7a3
Rahul Pal <rahulpal.dav@gmail.com>
2018-10-06 17:48:50 +0530

    error handlers and modified grid in BS

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const express = require('express');
 const path = require('path');
 const request = require('request');
@@ -24,38 +22,47 @@ app.get('/weather', (req, res) => {
     
     let location = req.query.address;
     location = encodeURIComponent(location);
-    // console.log(location);
+    console.log(location);
     
     request({
         url: `https://us1.locationiq.com/v1/search.php?key=0c49d4b22f610c&q=${location}&format=json`,
 		json: true,
     },(error, response, body) => {
-        results = {
-            lat: body[0].lat,
-            lon: body[0].lon,
-            loc: body[0].display_name,
-        }
-        // console.log(results.lat, results.lon);
-
-        request({
-        url: `https://api.darksky.net/forecast/a4a1eece83cc4d9087a11e07c1ff144d/${results.lat},${results.lon}`,
-        json: true,
-        }, (error, response, body) => {
-            res.render('weather.hbs', {
-                areaCaptured: results.loc,
-                temperature: Math.round((body.currently.temperature - 32) * (5/9)) + ' °C',
-                apparentTemperature: Math.round((body.currently.apparentTemperature - 32) * (5/9)) + ' °C',
-                summary: body.currently.summary,
-                humidity: (Math.round(body.currently.humidity*100) + '%'),
-                forecast: body.hourly.summary,
-                windSpeed: (Math.round((body.currently.windSpeed * 1.7)) + ' KPh'),
-            });
-        });
-
         
-    });
-    
+        if (error) {
+            res.render('ehandler.hbs', {
+                pageTitle: 'Location API Error!',
+                message: 'Unable to connect to Location API, please go back and try again!'
+            });
+        } else if (body.error === 'Unable to geocode') {
+            res.render('ehandler.hbs', {
+                pageTitle: 'Unable to get Geocode!',
+                message: 'Please check the PIN code or the address that you have entered and try again!'
+            });
+        } else if (response.statusCode === 200) {
+            results = {
+                lat: body[0].lat,
+                lon: body[0].lon,
+                loc: body[0].display_name,
+            }
+            request({
+            url: `https://api.darksky.net/forecast/a4a1eece83cc4d9087a11e07c1ff144d/${results.lat},${results.lon}`,
+            json: true,
+            }, (error, response, body) => {
+                res.render('weather.hbs', {
+                    areaCaptured: results.loc,
+                    temperature: Math.round((body.currently.temperature - 32) * (5/9)) + ' °C',
+                    apparentTemperature: Math.round((body.currently.apparentTemperature - 32) * (5/9)) + ' °C',
+                    summary: body.currently.summary,
+                    humidity: (Math.round(body.currently.humidity*100) + '%'),
+                    forecast: body.hourly.summary,
+                    windSpeed: (Math.round((body.currently.windSpeed * 1.7)) + ' KPh'),
+                });
+            });  
+        }
+    }); 
 });
+
 
 app.listen(port, () => {
 	console.log(`Server running in port ${port}`);
